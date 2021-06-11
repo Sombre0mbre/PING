2e2fef19e6d299d50253599637d4fd7af2b9fd19
cloe escudier <cloe.escudier@epita.fr>
2021-06-11 19:13:56 +0200

    fix maven features

--- src/main/java/fr/epita/assistants/myide/domain/entity/features/maven/PackageImplementation.java
+++ src/main/java/fr/epita/assistants/myide/domain/entity/features/maven/PackageImplementation.java
@@ -15,12 +15,12 @@ public class PackageImplementation implements Feature {
      */
     @Override
     public ExecutionReport execute(Project project, Object... params) {
-        ProcessBuilder pb = new ProcessBuilder("mvn", "package", Arrays.toString(params));
-        pb.directory(project.getRootNode().getPath().toFile());
+        ProcessBuilder pb = new ProcessBuilder("mvn", "package", Arrays.toString(params))
+                .directory(project.getRootNode().getPath().toFile());
         try {
             Process process = pb.start();
-            int exitCode = process.waitFor();
-            if (exitCode == 0)
+            process.waitFor();
+            if (process.exitValue() == 0)
                 return () -> true;
             else
                 return () -> false;
