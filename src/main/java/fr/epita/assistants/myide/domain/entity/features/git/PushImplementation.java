af9b24a1690d97bc11fb5350b38b17ed3ebb9689
cloe escudier <cloe.escudier@epita.fr>
2021-06-11 12:38:56 +0200

    push implementation ?

--- src/main/java/fr/epita/assistants/myide/domain/entity/features/git/PushImplementation.java
+++ src/main/java/fr/epita/assistants/myide/domain/entity/features/git/PushImplementation.java
@@ -3,6 +3,14 @@ package fr.epita.assistants.myide.domain.entity.features.git;
 import fr.epita.assistants.myide.domain.entity.Feature;
 import fr.epita.assistants.myide.domain.entity.Mandatory;
 import fr.epita.assistants.myide.domain.entity.Project;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class PushImplementation implements Feature {
     /**
@@ -12,7 +20,28 @@ public class PushImplementation implements Feature {
      */
     @Override
     public ExecutionReport execute(Project project, Object... params) {
-        throw new UnsupportedOperationException("FIXME");
+        FileRepositoryBuilder builder = new FileRepositoryBuilder();
+        Repository repository = null;
+        try {
+            repository = builder.setGitDir(new File(String.valueOf(project.getRootNode().getPath())))
+                    .readEnvironment()
+                    .findGitDir()
+                    .build();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return () -> false;
+        }
+
+        assert repository != null;
+        Git git = new Git(repository);
+        try {
+            git.push().call();
+            return () -> true;
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+            return () -> false;
+        }
+        //throw new UnsupportedOperationException("FIXME");
         /*
         int returnCode = exec("mvn", "compile");
         return () -> (returnCode == 0);
