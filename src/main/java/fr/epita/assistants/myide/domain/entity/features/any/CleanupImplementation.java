062c8539cb76206cc5a2eb083411fd15750c9fa0
aurelien.visentin <aurelien.visentin@epita.fr>
2021-06-12 18:17:26 +0200

    Cleanup

--- src/main/java/fr/epita/assistants/myide/domain/entity/features/any/CleanupImplementation.java
+++ src/main/java/fr/epita/assistants/myide/domain/entity/features/any/CleanupImplementation.java
@@ -3,6 +3,17 @@ package fr.epita.assistants.myide.domain.entity.features.any;
 import fr.epita.assistants.myide.domain.entity.Feature;
 import fr.epita.assistants.myide.domain.entity.Mandatory;
 import fr.epita.assistants.myide.domain.entity.Project;
+import org.apache.commons.io.FileUtils;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
 
 public class CleanupImplementation implements Feature {
     /**
@@ -12,7 +23,30 @@ public class CleanupImplementation implements Feature {
      */
     @Override
     public ExecutionReport execute(Project project, Object... params) {
-        throw new UnsupportedOperationException("FIXME");
+        var ideIgnore = project.getRootNode().getPath().resolve(".myideignore");
+        if (!ideIgnore.toFile().exists() || !ideIgnore.toFile().isFile())
+            return () -> false;
+        try {
+            var lines = new HashSet<>(Files.readAllLines(ideIgnore));
+
+            Files.walkFileTree(project.getRootNode().getPath(), new SimpleFileVisitor<>() {
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    if (lines.contains(file.getFileName().toString())) {
+                        if (file.toFile().isDirectory()) {
+                            FileUtils.deleteDirectory(file.toFile());
+                        } else {
+                            Files.delete(file);
+                        }
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+
+            return () -> true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return () -> false;
+        }
     }
 
     /**
---
+++ src/test/java/fr/epita/assistants/myide/domain/entity/features/any/CleanupImplementationTest.java
@@ -0,0 +1,34 @@
+package fr.epita.assistants.myide.domain.entity.features.any;
+
+import fr.epita.assistants.myide.domain.entity.Mandatory;
+import fr.epita.assistants.myide.domain.service.ProjectServiceImplementationTest;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CleanupImplementationTest {
+    @Test
+    void execute() throws IOException {
+        var project = new ProjectServiceImplementationTest().setUpDummy();
+        System.out.println(project);
+        var feature = project.getFeature(Mandatory.Features.Any.CLEANUP);
+        assert feature.isPresent();
+        assert !feature.get().execute(project).isSuccess();
+        Files.createFile(project.getRootNode().getPath().resolve("toto.txt"));
+        var ide = project.getRootNode().getPath().resolve(".myideignore").toFile();
+        Files.write(ide.toPath(), "toto.txt\n".getBytes(), StandardOpenOption.CREATE_NEW);
+        assert feature.get().execute(project).isSuccess();
+        var root =project.getRootNode().getPath().toFile();
+        for (File file : Objects.requireNonNull(root.listFiles())) {
+            System.out.println(file);
+        }
+        assert Objects.requireNonNull(root.listFiles()).length == 2;
+
+    }
+}
