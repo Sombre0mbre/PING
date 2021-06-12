f59c1ab970746df1da54d6fb2b4604968437fbc6
aurelien.visentin <aurelien.visentin@epita.fr>
2021-06-12 20:11:23 +0200

    ZIP ?

--- src/main/java/fr/epita/assistants/myide/domain/entity/features/any/DistImplementation.java
+++ src/main/java/fr/epita/assistants/myide/domain/entity/features/any/DistImplementation.java
@@ -3,10 +3,14 @@ package fr.epita.assistants.myide.domain.entity.features.any;
 import fr.epita.assistants.myide.domain.entity.Feature;
 import fr.epita.assistants.myide.domain.entity.Mandatory;
 import fr.epita.assistants.myide.domain.entity.Project;
+import org.apache.commons.compress.archivers.ArchiveException;
+import org.apache.commons.compress.archivers.ArchiveOutputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
@@ -14,29 +18,41 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class DistImplementation implements Feature {
-    private void zipDirContent(Path rootPath, File dir, ZipOutputStream zipOutputStream) throws IOException {
+    private void zipDirContent(Path rootPath, File dir, ArchiveOutputStream archive) throws IOException {
         if (!dir.isDirectory())
             return;
 
         for (var i : Objects.requireNonNull(dir.listFiles())) {
-            Path name;
+            StringBuilder pathStr = new StringBuilder(rootPath.relativize(i.toPath()).toString());
             if (i.isDirectory()) {
-                StringBuilder pathStr = new StringBuilder(rootPath.relativize(i.toPath()).toString());
                 if (!pathStr.toString().endsWith("/"))
                     pathStr.append("/");
 
-                zipOutputStream.putNextEntry(new ZipEntry(pathStr.toString()));
-                zipOutputStream.closeEntry();
+                ZipArchiveEntry entry = new ZipArchiveEntry(pathStr.toString());
+                archive.putArchiveEntry(entry);
+                archive.closeArchiveEntry();
 
-                zipDirContent(rootPath, i, zipOutputStream);
-            } else if (i.isFile()) {
-                zipOutputStream.putNextEntry(new ZipEntry(rootPath.relativize(i.toPath()).toString()));
-                Files.copy(i.toPath(), zipOutputStream);
-                zipOutputStream.closeEntry();
+                zipDirContent(rootPath, i, archive);
+            } else {
+                ZipArchiveEntry entry = new ZipArchiveEntry(pathStr.toString());
+                archive.putArchiveEntry(entry);
+                BufferedInputStream input = new BufferedInputStream(new FileInputStream(i));
+
+                IOUtils.copy(input, archive);
+                input.close();
+
+                archive.closeArchiveEntry();
             }
         }
     }
 
+
+    private String getName(Path rootPath, File i) {
+        StringBuilder pathStr = new StringBuilder(rootPath.relativize(i.toPath()).toString());
+        if (i.isDirectory() && !pathStr.toString().endsWith("/"))
+            pathStr.append("/");
+        return pathStr.toString();
+    }
 
     /**
      * @param project {@link Project} on which the feature is executed.
@@ -59,13 +75,14 @@ public class DistImplementation implements Feature {
         var zipPath = project.getRootNode().getPath().getParent().resolve(name);
         try {
             var outputStream = new FileOutputStream(zipPath.toFile());
-            var zipOutputStream = new ZipOutputStream(outputStream);
+            var archive = new ArchiveStreamFactory().createArchiveOutputStream(ArchiveStreamFactory.ZIP, outputStream);
 
-            zipDirContent(rootPath, rootPath.toFile(), zipOutputStream);
+            zipDirContent(rootPath, rootPath.toFile(), archive);
 
-            zipOutputStream.close();
+            archive.finish();
+            outputStream.close();
             return () -> true;
-        } catch (IOException e) {
+        } catch (IOException | ArchiveException e) {
             e.printStackTrace();
             return () -> false;
         }
