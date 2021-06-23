563cb0c50a4e852a4ee17f76d83a58d3de1b2ba7
aurelien.visentin <aurelien.visentin@epita.fr>
2021-06-23 12:03:05 +0200

    Gui

---
+++ src/main/java/fr/epita/assistants/Main.java
@@ -0,0 +1,21 @@
+package fr.epita.assistants;
+
+import fr.epita.assistants.myide.domain.gui.MyIdeGui;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        var mainFrame = new JFrame("myIDE");
+        mainFrame.setContentPane(new MyIdeGui().mainPanel);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Continuing with default value");
+        }
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+    }
+}
