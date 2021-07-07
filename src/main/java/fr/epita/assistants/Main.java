f41e78ba4003d80f7217213a0a86be390ece8950
aurelien.visentin <aurelien.visentin@epita.fr>
2021-07-07 18:22:06 +0200

    Gui dark mode

--- src/main/java/fr/epita/assistants/Main.java
+++ src/main/java/fr/epita/assistants/Main.java
@@ -1,11 +1,14 @@
 package fr.epita.assistants;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
 import fr.epita.assistants.myide.domain.gui.MyIdeGui;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        FlatDarkLaf.setup();
         var mainFrame = new JFrame("myIDE");
         mainFrame.setContentPane(new MyIdeGui().mainPanel);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
---
+++ src/main/java/fr/epita/assistants/myide/domain/gui/Startup.java
@@ -0,0 +1,10 @@
+package fr.epita.assistants.myide.domain.gui;
+
+import javax.swing.*;
+
+public class Startup {
+    private JButton ouvrirUnProjetButton;
+    private JPanel panel1;
+    private JButton nouveauProjetButton;
+    private JTextPane bonjourEtBienvenueDansTextPane;
+}
