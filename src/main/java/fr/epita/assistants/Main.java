caff6ee9e15821b4eca120d2685217121aecd578
aurelien.visentin <aurelien.visentin@epita.fr>
2021-07-08 21:14:46 +0200

    Fix

--- src/main/java/fr/epita/assistants/Main.java
+++ src/main/java/fr/epita/assistants/Main.java
@@ -1,24 +1,9 @@
 package fr.epita.assistants;
 
-import com.formdev.flatlaf.FlatDarkLaf;
 import fr.epita.assistants.myide.domain.javafx.ApplicationMain;
 
 public class Main {
     public static void main(String[] args) {
-        FlatDarkLaf.setup();
-        /*
-        var mainFrame = new JFrame("myIDE");
-        mainFrame.setContentPane(new MyIdeGui().mainPanel);
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println("Continuing with default value");
-        }
-        mainFrame.pack();
-        mainFrame.setVisible(true);
-        */
         ApplicationMain.main(args);
     }
 }
--- src/main/java/fr/epita/assistants/myide/domain/javafx/TutorialController.java
+++ src/main/java/fr/epita/assistants/myide/domain/javafx/TutorialController.java
@@ -1,5 +1,6 @@
 package fr.epita.assistants.myide.domain.javafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
@@ -12,9 +13,32 @@ public class TutorialController {
     public Button mavenButton;
     public TextArea tutorialText;
 
+    final String[] gitButtonsNames = {"Push", "Pull", "Add", "Commit"};
+    final String[] mavenButtonsNames = {"Compile", "Clean", "Exec", "Install", "Package", "Test", "Tree"};
+    final String[] javaButtonsNames = {"if", "Boucles", "Fonctions", "Classes"};
+
     @FXML
     public void initialize() {
         tutorialText.setText("");
+    }
+
+    public void gitButtonAction(ActionEvent actionEvent) {
+
+    }
+
+    public void mavenButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void javaButtonAction(ActionEvent actionEvent) {
+    }
+
+    private void setFlowPaneFromArray(String[] array) {
+        flowPane.getChildren().clear();
+        for (var i : array) {
+            var button = new Button(i);
+
+            flowPane.getChildren().add(button);
+        }
     }
 
     // TODO - Add buttons dynamically to flowPane and set text accordingly
