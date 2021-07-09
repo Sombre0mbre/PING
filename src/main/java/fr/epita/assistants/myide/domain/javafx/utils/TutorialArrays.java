9b226f5322f7af0a129afa58dce3c01627603818
aurelien.visentin <aurelien.visentin@epita.fr>
2021-07-09 18:30:17 +0200

    Gui changes

--- src/main/java/fr/epita/assistants/myide/domain/javafx/utils/TutorialArrays.java
+++ src/main/java/fr/epita/assistants/myide/domain/javafx/utils/TutorialArrays.java
@@ -2,24 +2,63 @@ package fr.epita.assistants.myide.domain.javafx.utils;
 
 public class TutorialArrays {
     public final static TutorialElement[] gitElements = {
-            new TutorialElement("Push", "Description 1"),
-            new TutorialElement("Add", "Description 2"),
-            new TutorialElement("Commit", "Description 3")
+            new TutorialElement(
+                    "Push",
+                    "Description 1"
+            ),
+            new TutorialElement(
+                    "Add",
+                    "Description 2"
+            ),
+            new TutorialElement(
+                    "Commit",
+                    "Description 3"
+            )
     };
 
     public final static TutorialElement[] mavenElements = {
-            new TutorialElement("Compile", "Description 4"),
-            new TutorialElement("Clean", "Description 5"),
-            new TutorialElement("Exec", "Description 6"),
-            new TutorialElement("Install", "Description 7"),
-            new TutorialElement("Test", "Description 8"),
-            new TutorialElement("Tree", "Description 9"),
+            new TutorialElement(
+                    "Compile",
+                    "Description 4"
+            ),
+            new TutorialElement(
+                    "Clean",
+                    "Description 5"
+            ),
+            new TutorialElement(
+                    "Exec",
+                    "Description 6"
+            ),
+            new TutorialElement(
+                    "Install",
+                    "Description 7"
+            ),
+            new TutorialElement(
+                    "Test",
+                    "Description 8"
+            ),
+            new TutorialElement(
+                    "Tree",
+                    "Description 9"
+            )
     };
 
     public final static TutorialElement[] javaElements = {
-            new TutorialElement("Conditions", "Description 10"),
-            new TutorialElement("Boucles", "Description 11"),
-            new TutorialElement("Fonctions", "Description 12"),
-            new TutorialElement("Classes", "Description 13"),
+            new TutorialElement(
+                    "Conditions",
+                    "Description 10"
+            ),
+            new TutorialElement(
+                    "Boucles",
+                    "Description 11"
+            ),
+            new TutorialElement(
+                    "Fonctions",
+                    "Description 12"
+            ),
+            new TutorialElement(
+                    "Classes",
+                    "Description 13"
+            )
     };
 }
