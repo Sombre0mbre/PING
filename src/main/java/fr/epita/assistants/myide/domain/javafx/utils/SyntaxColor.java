a074051667704c34b6b584a66e61bbbbc8cbb744
aurelien.visentin <aurelien.visentin@epita.fr>
2021-07-11 01:31:59 +0200

    Simplifications

--- src/main/java/fr/epita/assistants/myide/domain/javafx/utils/SyntaxColor.java
+++ src/main/java/fr/epita/assistants/myide/domain/javafx/utils/SyntaxColor.java
@@ -16,18 +16,13 @@ import java.util.regex.Pattern;
 public class SyntaxColor {
 
     static final String[] keyWords = new String[]{
-            "abstract", "assert", "boolean", "break", "byte",
-            "case", "catch", "char", "class", "const",
-            "continue", "default", "do", "double", "else",
-            "enum", "extends", "final", "finally", "float",
-            "for", "goto", "if", "implements", "import",
-            "instanceof", "int", "interface", "long", "native",
-            "new", "package", "private", "protected", "public",
-            "return", "short", "static", "strictfp", "super",
-            "switch", "synchronized", "this", "throw", "throws",
-            "transient", "try", "void", "volatile", "while", "var"
+            "abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class", "const",
+            "continue", "default", "do", "double", "else", "enum", "extends", "final", "finally", "float",
+            "for", "goto", "if", "implements", "import", "instanceof", "int", "interface", "long", "native",
+            "new", "package", "private", "protected", "public", "return", "short", "static", "strictfp", "super",
+            "switch", "synchronized", "this", "throw", "throws", "transient", "try", "void", "volatile", "while", "var",
+            "true", "false", "record", "non-sealed", "permits", "sealed", "yield", "null"
     };
-
     static final String keywordRegex = "\\b(" + String.join("|", keyWords) + ")\\b";
     static final String parenRegex = "[()]";
     static final String braceRegex = "[{}]";
