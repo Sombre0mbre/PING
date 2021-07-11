7b7e1ed0d6929f7cabcbb505baadefc20fec4d13
aurelien.visentin <aurelien.visentin@epita.fr>
2021-07-11 13:13:35 +0200

    Icon

--- src/main/java/fr/epita/assistants/myide/domain/javafx/ApplicationMain.java
+++ src/main/java/fr/epita/assistants/myide/domain/javafx/ApplicationMain.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,7 +19,10 @@ public class ApplicationMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-
+        primaryStage.getIcons().add(
+                new Image(
+                        Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("icons/icon.png"))));
         SceneLoader.loadStartup(primaryStage);
+
     }
 }
