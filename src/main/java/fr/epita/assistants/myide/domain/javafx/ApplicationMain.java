92ec9bbb894d8f48f6006831a526e57d29180800
aurelien.visentin <aurelien.visentin@epita.fr>
2021-07-11 15:20:24 +0200

    Fix

--- src/main/java/fr/epita/assistants/myide/domain/javafx/ApplicationMain.java
+++ src/main/java/fr/epita/assistants/myide/domain/javafx/ApplicationMain.java
@@ -2,11 +2,14 @@ package fr.epita.assistants.myide.domain.javafx;
 
 import fr.epita.assistants.myide.domain.javafx.utils.SceneLoader;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -23,6 +26,10 @@ public class ApplicationMain extends Application {
                 new Image(
                         Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("icons/icon.png"))));
         SceneLoader.loadStartup(primaryStage);
+        primaryStage.setOnCloseRequest(t -> {
+            Platform.exit();
+            System.exit(0);
+        });
 
     }
 }
